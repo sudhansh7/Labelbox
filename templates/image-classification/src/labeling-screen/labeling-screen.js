20e4057a619b717b45de4169468e3494f965cda1
Daniel Rasmuson <dan123911@gmail.com>
2018-01-07 15:09:50 -0800

    Adding disabled state to next

--- templates/image-classification/src/labeling-screen/labeling-screen.js
+++ templates/image-classification/src/labeling-screen/labeling-screen.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from 'material-ui/Button';
 import ClassificationForm from './classification-options';
 
-export function LabelingScreen(props) {
-  if (!props.imageUrl) {
-    return (<div>Loading...</div>);
-  }
+export class LabelingScreen extends Component {
+  render() {
+    if (!this.props.imageUrl) {
+      return (<div>Loading...</div>);
+    }
 
-  return (
-    <div>
+    let chosenLabel;
+
+    return (
       <div>
-        <img src={props.imageUrl} alt="classify-data" />
-      </div>
-      <div className="form-controls">
-        <div className="classification">
-          <ClassificationForm />
+        <div>
+          <img src={this.props.imageUrl} alt="classify-data" />
         </div>
-        <div className="form-buttons">
-          <Button>Skip</Button>
-          <Button raised={true} color="primary">Submit</Button>
+        <div className="form-controls">
+          <div className="classification">
+            <ClassificationForm />
+          </div>
+          <div className="form-buttons">
+            <Button>Skip</Button>
+            <Button raised={true} color="primary" disabled={!chosenLabel}>Submit</Button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
