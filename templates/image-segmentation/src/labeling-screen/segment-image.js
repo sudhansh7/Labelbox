6a18e46e401a43761b8ed584861d955b4eec5cb3
Daniel Rasmuson <dan123911@gmail.com>
2018-01-07 18:29:55 -0800

    Added leaflet draw

--- templates/image-segmentation/src/labeling-screen/segment-image.js
+++ templates/image-segmentation/src/labeling-screen/segment-image.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
 import './leaflet.css';
+import './leaflet-draw/leaflet.draw.css';
+import './leaflet-draw/leaflet.draw';
+
+function getSizeOnImage(url) {
+  return new Promise((resolve) => {
+    const img = document.createElement('img');
+    img.src = url;
+    img.onload = (event) => {
+      document.body.removeChild(img);
+      resolve({
+        width: img.naturalWidth,
+        height: img.naturalHeight
+      });
+    };
+    img.style.display = 'none';
+    document.body.appendChild(img);
+  })
+}
 
 export class SegmentImage extends Component {
   componentDidMount(){
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    const { imageUrl } = this.props;
+    getSizeOnImage(imageUrl).then((size) => this.drawMap({imageUrl, ...size}));
   }
+
+  drawMap({imageUrl, width, height}) {
+    const map = L.map('map', {
+      crs: L.CRS.Simple,
+      minZoom: -5,
+      attributionControl: false,
+      zoomControl: false
+    });
+    const bounds = [[0,0], [height,width]];
+    const image = L.imageOverlay(imageUrl, bounds).addTo(map);
+    map.fitBounds(bounds);
+    map.setZoom(-1)
+    console.log(map)
+  }
+
 
   render() {
     return (
       <div>
-        <div id="mapid" style={{height: '180px'}}></div>
-        <div>Segment this image {this.props.imageUrl}</div>
+        <div id="map" style={{height: '350px'}}></div>
       </div>
     );
   }
