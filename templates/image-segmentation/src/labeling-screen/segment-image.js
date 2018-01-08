c371a047953e54ec9cd316cb740fbd8eb573c56e
Daniel Rasmuson <dan123911@gmail.com>
2018-01-07 17:49:14 -0800

    Adding leaflet

--- templates/image-segmentation/src/labeling-screen/segment-image.js
+++ templates/image-segmentation/src/labeling-screen/segment-image.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import L from 'leaflet';
+import './leaflet.css';
 
-export function SegmentImage(props) {
-  return (<div>Segment this image {props.imageUrl}</div>);
+export class SegmentImage extends Component {
+  componentDidMount(){
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="mapid" style={{height: '180px'}}></div>
+        <div>Segment this image {this.props.imageUrl}</div>
+      </div>
+    );
+  }
 }
