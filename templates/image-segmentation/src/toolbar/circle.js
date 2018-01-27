5b8ee8d78e61c141985d19477c5da686bcade625
Daniel Rasmuson <dan123911@gmail.com>
2018-01-26 20:15:25 -0800

    Moved to only ts

--- templates/image-segmentation/src/toolbar/circle.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export function Circle({color}) {
-  return (
-    <div style={{borderRadius: '100%', width: '25px', height: '25px', backgroundColor: color}}></div>
-  );
-}
