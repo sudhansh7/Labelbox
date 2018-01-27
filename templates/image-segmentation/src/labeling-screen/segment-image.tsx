0a303cd56af2ade4626306889bbf00e4b0b701d3
Daniel Rasmuson <dan123911@gmail.com>
2018-01-26 21:38:24 -0800

    styles changes

--- templates/image-segmentation/src/labeling-screen/segment-image.tsx
+++ templates/image-segmentation/src/labeling-screen/segment-image.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 /* import { LinearProgress } from 'material-ui/Progress';*/
 /* import Icon from 'material-ui/Icon';*/
-import { Map, Marker, Popup } from 'react-leaflet';
+import { Map, ImageOverlay } from 'react-leaflet';
 import { CRS } from 'leaflet';
 
 interface Props {
@@ -15,22 +15,20 @@ interface Props {
 }
 
 export function SegmentImage ({imageUrl, imageSize: {width, height}}: Props) {
-  const position = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13
-  };
 
   // tslint:disable-next-line
-  console.log('render this image', imageUrl, width, height);
+  console.log('render th s image', imageUrl, width, height);
 
+  // TODO improve zooming
   return (
-    <Map zoom={position.zoom} crs={CRS.Simple} bounds={[[0, 0], [height, width]]}>
-      <Marker position={position}>
-        <Popup>
-          <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-        </Popup>
-      </Marker>
+    <Map
+      crs={CRS.Simple}
+      bounds={[[0, 0], [height, width]]}
+      maxZoom={100}
+      minZoom={-4}
+      zoomControl={false}
+    >
+      <ImageOverlay url={imageUrl} bounds={[[0, 0], [height, width]]}/>
     </Map>
   );
 }
