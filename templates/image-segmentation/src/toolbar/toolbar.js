9a7c279439d80c11df3c0a85678f95083c86a440
Daniel Rasmuson <dan123911@gmail.com>
2018-01-26 19:08:18 -0800

    All components except app should be a function

--- templates/image-segmentation/src/toolbar/toolbar.js
+++ templates/image-segmentation/src/toolbar/toolbar.js
@@ -2,41 +2,37 @@ import React, { Component } from 'react';
 import { Tool } from './tool';
 import Button from 'material-ui/Button';
 
-export class Toolbar extends React.Component {
-  render() {
-    const { tools, toolChange, currentTool } = this.props;
-
-    return (
-      <div className="toolbar">
-        <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
-          <div style={{margin: '20px 15px 10px', fontWeight: '700'}}>Select a class below</div>
-          {
-            tools.map(({name, color}, index) => (
-              <Tool
-                key={index}
-                name={name}
-                color={color}
-                count={1}
-                onClick={() => toolChange(index)}
-                selected={index === currentTool}
-              />
-            ))
-          }
-        </div>
-        <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
-          <div style={{margin: '20px 15px 10px', fontWeight: '700'}}>Keyboard shortcuts</div>
-          <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'}}>
-            <div style={{display: 'flex', flexGrow: '1'}}>
-              <div style={{flex: '60'}}>Toggle Annotation</div>
-              <div style={{flex: '40'}}>Spacebar</div>
-            </div>
+export function Toolbar({tools, toolChange, currentTool}) {
+  return (
+    <div className="toolbar">
+      <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
+        <div style={{margin: '20px 15px 10px', fontWeight: '700'}}>Select a class below</div>
+        {
+          tools.map(({name, color}, index) => (
+            <Tool
+              key={index}
+              name={name}
+              color={color}
+              count={1}
+              onClick={() => toolChange(index)}
+              selected={index === currentTool}
+            />
+          ))
+        }
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
+        <div style={{margin: '20px 15px 10px', fontWeight: '700'}}>Keyboard shortcuts</div>
+        <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'}}>
+          <div style={{display: 'flex', flexGrow: '1'}}>
+            <div style={{flex: '60'}}>Toggle Annotation</div>
+            <div style={{flex: '40'}}>Spacebar</div>
           </div>
         </div>
-        <div style={{display: 'flex', flexGrow: '1', flexDirection: 'column'}}></div>
-        <div style={{display: 'flex', justifyContent: 'flex-end', margin: '15px'}}>
-          <Button color="primary" raised={true}>Submit</Button>
-        </div>
       </div>
-    );
-  }
+      <div style={{display: 'flex', flexGrow: '1', flexDirection: 'column'}}></div>
+      <div style={{display: 'flex', justifyContent: 'flex-end', margin: '15px'}}>
+        <Button color="primary" raised={true}>Submit</Button>
+      </div>
+    </div>
+  );
 }
