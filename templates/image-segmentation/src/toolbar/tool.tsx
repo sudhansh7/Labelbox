e8521067641e92509926e02177c142e2d5b2365c
Daniel Rasmuson <dan123911@gmail.com>
2018-02-03 17:19:04 -0800

    Fix to segmentation buttons

--- templates/image-segmentation/src/toolbar/tool.tsx
+++ templates/image-segmentation/src/toolbar/tool.tsx
@@ -17,59 +17,73 @@ export function Tool(
     visibilityToggle: () => void
   }
 ) {
-  const handleClick = (e: any) => {
-    if (e.target.classList.contains('material-icons')) {
-      visibilityToggle()
-    } else {
-      onClick()
-    }
-  }
-
   return (
-    <Button
-      style={{
-        display: 'flex',
-        flexGrow: '1',
-        alignItems: 'center',
-        width: '100%',
-        padding: '10px 0px',
-        paddingBottom: selected ? '5px' : '10px',
-        cursor: 'pointer',
-        textTransform: 'none',
-        borderRadius: '0px',
-        borderBottom: '1px solid #c1c1c1'
-        // tslint:disable-next-line
-      } as any}
-      color={selected ? 'primary' : 'inherit'}
-      raised={selected}
-      onClick={handleClick}
-    >
-      <div
-        style={{display: 'flex', flexGrow: '1', alignItems: 'center'} as any}
-      >
-        <div style={{display: 'flex', flexDirection: 'column', marginLeft: '15px', lineHeight: '15px', alignItems: 'start'} as any}>
-          <div style={{display: 'flex'}}>
-            <div >{name}</div>
-            <div style={{marginLeft: '5px'}}>({count})</div>
-          </div>
-          {
-            selected && <div style={{fontSize: '9px'}}>
-              Draw a {toolName}
-            </div>
-          }
-        </div>
-        {/* tslint:disable-next-line */}
-        <div style={{display: 'flex', flexGrow: '1'} as any}></div>
-      </div>
-      <Icon
+    <div style={{
+      display: 'flex',
+      flexGrow: '1',
+    } as any}>
+      <Button
         style={{
-          color: selected ? 'white' : color,
-          marginRight: '20px',
-          opacity: visible ? 1 : 0.2
-        }}
+          width: '50px',
+          minWidth: '50px',
+          padding: '10px 0px',
+          paddingBottom: '5px',
+          cursor: 'pointer',
+          textTransform: 'none',
+          borderRadius: '0px',
+          borderBottom: '1px solid #c1c1c1',
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: 'none',
+          // tslint:disable-next-line
+        } as any}
+        onClick={visibilityToggle}
       >
-        visibility
-      </Icon>
-    </Button>
+        <Icon
+          style={{
+            color: visible ? color : 'black',
+            opacity: visible ? 1 : 0.2
+          }}
+        >
+          visibility
+        </Icon>
+      </Button>
+      <Button
+        style={{
+          display: 'flex',
+          flexGrow: '1',
+          alignItems: 'center',
+          width: '100%',
+          padding: '10px 0px',
+          paddingBottom: '5px',
+          cursor: 'pointer',
+          textTransform: 'none',
+          borderRadius: '0px',
+          borderBottom: '1px solid #c1c1c1'
+          // tslint:disable-next-line
+        } as any}
+        color={selected ? 'primary' : 'inherit'}
+        raised={selected}
+        onClick={() => onClick()}
+      >
+        <div
+          style={{display: 'flex', flexGrow: '1', alignItems: 'center'} as any}
+        >
+          <div style={{display: 'flex', flexDirection: 'column', marginLeft: '15px', lineHeight: '15px', alignItems: 'start'} as any}>
+            <div style={{display: 'flex'}}>
+              <div >{name}</div>
+              <div style={{marginLeft: '5px'}}>({count})</div>
+            </div>
+            {
+              selected && <div style={{fontSize: '9px'}}>
+                Draw a {toolName}
+              </div>
+            }
+          </div>
+          {/* tslint:disable-next-line */}
+          <div style={{display: 'flex', flexGrow: '1'} as any}></div>
+        </div>
+      </Button>
+    </div>
   );
 }
