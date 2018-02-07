f22c5a5fb4bef8f2a8bbd1c04e4cf0068f2c1711
Daniel Rasmuson <dan123911@gmail.com>
2018-02-06 19:25:26 -0800

    Fix for deploying templates

--- templates/image-classification/src/index.js
+++ templates/image-classification/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// You can remove this at the end of febuary 2018
+// we had a service worker for a while and had chaching problems
+// this file unregister previsouly made service workers
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+  for(let registration of registrations) {
+    registration.unregister();
+  }
+});
--- templates/image-segmentation/src/index.tsx
+++ templates/image-segmentation/src/index.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// You can remove this at the end of febuary 2018
+// we had a service worker for a while and had chaching problems
+// this file unregister previsouly made service workers
+navigator.serviceWorker.getRegistrations().then(function(registrations: any) {
+ for(let registration of registrations) {
+  registration.unregister()
+ }
+});
