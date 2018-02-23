d932cffdb5800450824084d211688ae0dd641fb5
Daniel Rasmuson <dan123911@gmail.com>
2018-02-22 19:10:21 -0800

    added keyboard shortcut info

--- templates/image-segmentation/src/toolbar/toolbar.tsx
+++ templates/image-segmentation/src/toolbar/toolbar.tsx
@@ -44,42 +44,55 @@ export function Toolbar(
       <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
 
         <div style={{margin: '20px 15px 10px', fontWeight: '700'} as any}>Keyboard shortcuts</div>
-        <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'} as any}>
-          <div style={{display: 'flex', flexGrow: '1'} as any}>
-            <div style={{flex: '60'}}>Complete Shape</div>
-            <div style={{flex: '40'}}>enter</div>
+        <div style={{overflow: 'scroll'}}>
+          <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey', minHeight: '10px'} as any}>
+            <div style={{display: 'flex', flexGrow: '1'} as any}>
+              <div style={{flex: '60'}}>Complete Shape</div>
+              <div style={{flex: '40'}}>enter</div>
+            </div>
           </div>
-        </div>
-        <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'} as any}>
-          <div style={{display: 'flex', flexGrow: '1'} as any}>
-            <div style={{flex: '60'}}>Undo</div>
-            <div style={{flex: '40'}}>cmd/ctrl z</div>
+          <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey', minHeight: '10px'} as any}>
+            <div style={{display: 'flex', flexGrow: '1'} as any}>
+              <div style={{flex: '60'}}>Undo</div>
+              <div style={{flex: '40'}}>cmd/ctrl z</div>
+            </div>
           </div>
-        </div>
-        <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'} as any}>
-          <div style={{display: 'flex', flexGrow: '1'} as any}>
-            <div style={{flex: '60'}}>Delete</div>
-            <div style={{flex: '40'}}>backspace</div>
+          <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey', minHeight: '10px'} as any}>
+            <div style={{display: 'flex', flexGrow: '1'} as any}>
+              <div style={{flex: '60'}}>Delete</div>
+              <div style={{flex: '40'}}>backspace</div>
+            </div>
           </div>
-        </div>
-        <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'} as any}>
-          <div style={{display: 'flex', flexGrow: '1'} as any}>
-            <div style={{flex: '60'}}>Deselect Tool</div>
-            <div style={{flex: '40'}}>escape</div>
+          <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey', minHeight: '10px'} as any}>
+            <div style={{display: 'flex', flexGrow: '1'} as any}>
+              <div style={{flex: '60'}}>Deselect Tool</div>
+              <div style={{flex: '40'}}>escape</div>
+            </div>
           </div>
-        </div>
-        <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'} as any}>
-          <div style={{display: 'flex', flexGrow: '1'} as any}>
-            <div style={{flex: '60'}}>Submit</div>
-            <div style={{flex: '40'}}>cmd/ctrl s</div>
+          <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey', minHeight: '10px'} as any}>
+            <div style={{display: 'flex', flexGrow: '1'} as any}>
+              <div style={{flex: '60'}}>Submit</div>
+              <div style={{flex: '40'}}>cmd/ctrl s</div>
+            </div>
           </div>
-        </div>
-        <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey'} as any}>
-          <div style={{display: 'flex', flexGrow: '1'} as any}>
-            <div style={{flex: '60'}}>Skip</div>
-            <div style={{flex: '40'}}>cmd/ctrl d</div>
+          <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey', minHeight: '10px'} as any}>
+            <div style={{display: 'flex', flexGrow: '1'} as any}>
+              <div style={{flex: '60'}}>Skip</div>
+              <div style={{flex: '40'}}>cmd/ctrl d</div>
+            </div>
           </div>
+          {
+            tools.map(({name}, index) => (
+              <div style={{display: 'flex', flexGrow: '1', borderBottom: '1px solid #c1c1c1', fontSize: '12px', padding: '10px 15px', color: 'grey', minHeight: '10px'} as any}>
+                <div style={{display: 'flex', flexGrow: '1'} as any}>
+                  <div style={{flex: '60'}}>{name}</div>
+                  <div style={{flex: '40'}}>{index + 1}</div>
+                </div>
+              </div>
+            ))
+          }
         </div>
+
       </div>
       <div style={{display: 'flex', flexGrow: '1', flexDirection: 'column'} as any}></div>
       <div style={{display: 'flex', justifyContent: 'flex-end', margin: '15px'}}>
