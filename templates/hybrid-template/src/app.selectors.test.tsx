566c61d5b7f0fe6a989a9e66b40da42bc74ab6d5
Daniel Rasmuson <dan123911@gmail.com>
2018-04-05 18:27:30 -0700

    Made a todo to finish some tests

---
+++ templates/hybrid-template/src/app.selectors.test.tsx
@@ -0,0 +1,21 @@
+
+/* import {*/
+/* appReducer*/
+/* } from './app.reducer';*/
+/* import { selectIntentFromMapClick } from './app.selectors';*/
+
+// TODO finish these tests
+describe('app.selectors', () => {
+  describe('selectIntentFromMapClick', () => {
+    it('should complete the bounding box if the user is currently drawing', () => {
+    });
+    it('should deselect an annotation if they arent clicking a new shape', () => {
+    });
+    it('should select a shape if they click on it', () => {
+    });
+    it('should return undefined if the click is on a blank map', () => {
+      /* const action = selectIntentFromMapClick(appReducer(), {location: {lat: 1, lng: 1}})*/
+      /* expect(action).toEqual(undefined);*/
+    });
+  });
+});
