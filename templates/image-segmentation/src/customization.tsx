165d611de48e4d5bdd92f2f47450adaeace045b0
Daniel Rasmuson <dan123911@gmail.com>
2018-04-12 23:01:27 -0700

    fix for default setup

--- templates/image-segmentation/src/customization.tsx
+++ templates/image-segmentation/src/customization.tsx
@@ -4,31 +4,50 @@ enum FieldTypes {
 }
 
 export const screenText = {
-  tools: [
-    {name: 'Vegetation', color: 'pink', tool: 'polygon'},
-    {name: 'Paved Road', color: 'purple', tool: 'polygon'},
-    {name: 'Buildings', color: 'orange', tool: 'rectangle'},
-    {name: 'Sidewalk', color: 'green', tool: 'line'},
+  "tools": [
+    {"name": "Car", "color": "navy", "tool": "polygon"},
+    {"name": "Tree", "color": "green", "tool": "polygon"},
+    {"name": "Road Sign", "color": "orange", "tool": "polygon"},
+    {"name": "Person", "color": "pink", "tool": "rectangle"},
+    {"name": "Corner of Building", "color": "red", "tool": "point"}
   ],
-  classifications: [
+  "classifications": [
     {
-      name: 'model',
-      instructions: "Select the car model",
-      type: FieldTypes.RADIO,
-      options: [
-        { value: "model_s", label: "Tesla Model S"},
-        { value: "model_3", label: "Tesla Model 3"},
-        { value: "model_x", label: "Tesla Model X"},
+      "name": "model",
+      "instructions": "Select the car model",
+      "type": FieldTypes.RADIO,
+      "options": [
+        {
+          "value": "model_s",
+          "label": "Tesla Model S"
+        },
+        {
+          "value": "model_3",
+          "label": "Tesla Model 3"
+        },
+        {
+          "value": "model_x",
+          "label": "Tesla Model X"
+        }
       ]
     },
     {
-      name: 'image_problems',
-      instructions: "Select all that apply",
-      type: FieldTypes.CHECKLIST,
-      options: [
-        { value: "blur", label: "Blurry"},
-        { value: "saturated", label: "Over Saturated"},
-        { value: "pixelated", label: "Pixelated"},
+      "name": "image_problems",
+      "instructions": "Select all that apply",
+      "type": FieldTypes.CHECKLIST,
+      "options": [
+        {
+          "value": "blur",
+          "label": "Blurry"
+        },
+        {
+          "value": "saturated",
+          "label": "Over Saturated"
+        },
+        {
+          "value": "pixelated",
+          "label": "Pixelated"
+        }
       ]
     }
   ]
