38e36146b71f57d8f9394dacfadfc377a1510a41
Daniel Rasmuson <dan123911@gmail.com>
2018-04-18 22:21:54 -0700

    progress

--- docs/src/importing-data/importing-data.tsx
+++ docs/src/importing-data/importing-data.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import { Query } from '../query/query';
+import {stripIndent} from 'common-tags';
+
+const query = stripIndent`
+  query {
+    user {
+      id
+      email
+    }
+  }
+`
 
 export function ImportingData() {
   return (
-    <div>hi</div>
+    <div>
+      hi
+      <Query query={query}/>
+    </div>
   );
 }
