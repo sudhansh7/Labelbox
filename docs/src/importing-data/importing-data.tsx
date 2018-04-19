b0af803ce108fafb5b11444b46ac516a4edc1016
Daniel Rasmuson <dan123911@gmail.com>
2018-04-18 23:56:14 -0700

    Updates to docs

--- docs/src/importing-data/importing-data.tsx
+++ docs/src/importing-data/importing-data.tsx
@@ -2,25 +2,79 @@ import * as React from 'react';
 import { Query } from '../query/query';
 import {stripIndent} from 'common-tags';
 
-const query = stripIndent`
-  query {
-    user {
-      email
-    }
-  }
-`
 
 export function ImportingData() {
   return (
     <div style={{display: 'flex', flexGrow: 1, flexDirection: 'column'}}>
-      <div>Data Import Tutorial</div>
+      <div>Data Import Tutorial (beta)</div>
+      <div style={{color: 'grey'}}>The api calls below may change in the future.</div>
 
       <div>
-        All operations in labelbox occur through our <a href="https://graphql.org/">GraphQL API</a>.
+        All operations in labelbox go through our <a href="https://graphql.org/">GraphQL API</a>.
         Click the play button below to see an example query.
       </div>
 
-      <Query query={query} />
+      <Query query={stripIndent`
+          query {
+            user {
+              email
+            }
+          }
+        `}
+      />
+
+      <div>Pull the needed ID's for your data import query</div>
+      <Query query={stripIndent`
+          query {
+            user {
+              id
+              organization{
+                id
+                projects{
+                  id
+                  name
+                }
+                datasets{
+                  id
+                  name
+                }
+              }
+            }
+          }
+        `}
+      />
+
+      <div>Create a dataset</div>
+      <Query query={stripIndent`
+          mutation {
+            createDataset(
+              name: "<INSERT_NAME_HERE>",
+              projectsIds: ["<INSERT_PROJECT_ID_HERE>"],
+              createdById: "<INSERT_YOUR_USER_ID_FROM_ABOVE_HERE>",
+              organizationId: "<INSERT_YOUR_ORGANIZATION_ID_FROM_ABOVE_HERE>",
+              deleted: false,
+            ) {
+              id
+            }
+          }
+        `}
+      />
+
+      <div>Create a datarow in a dataset</div>
+      <Query query={stripIndent`
+          mutation {
+            createDataRow(
+              rowData: "<DATA_THAT_NEEDS_TO_BE_LABELED>",
+              datasetId: "<DATASET_ID_HERE>",
+              createdById: "<INSERT_YOUR_USER_ID_FROM_ABOVE_HERE>",
+              organizationId: "<INSERT_YOUR_ORGANIZATION_ID_FROM_ABOVE_HERE>",
+            ) {
+              id
+            }
+          }
+        `}
+      />
+
     </div>
   );
 }
