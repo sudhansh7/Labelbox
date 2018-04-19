2eb432805b5238740e031a7ff8bb48860eec452a
Daniel Rasmuson <dan123911@gmail.com>
2018-04-18 22:21:54 -0700

    progress

---
+++ docs/src/query/query.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import GraphiQL from 'graphiql';
+import 'graphiql/graphiql.css';
+
+function graphQLFetcher(graphQLParams: any) {
+  return fetch('https://api.labelbox.io/graphql', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
+
+export function Query({query}: {query: string}) {
+  return (
+    <GraphiQL fetcher={graphQLFetcher} query={query} />
+  );
+}
