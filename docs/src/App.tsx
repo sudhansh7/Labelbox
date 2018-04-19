5abbeccf80e670cd1f6aba24e14162276dab5845
Daniel Rasmuson <dan123911@gmail.com>
2018-04-18 21:52:56 -0700

    Progress

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,22 +1,66 @@
 import * as React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import { ImportingData } from './importing-data/importing-data';
 
-import logo from './logo.svg';
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  height: 100vh;
+`;
+
+// const Content = styled.div`
+//   display: flex;
+//   width: 980px;
+// `;
+
+// const MainContent = styled.div`
+//   display: flex;
+//   width: 980px;
+// `;
+
+const Menu = styled.div`
+  width: 260px;
+  padding: 40px;
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex: 35;
+  background-color: #f8f8f9;
+  justify-content: flex-end;
+`;
+
+const Right = styled.div`
+  display: flex;
+  flex: 65;
+`;
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <AppContainer>
+        <Left>
+          <Menu>
+            <div>Tutorials</div>
+            <div>Importing Data</div>
+          </Menu>
+        </Left>
+        <Right>
+          <ImportingData />
+        </Right>
+      </AppContainer>
     );
   }
 }
+
+        // <Content>
+        //   <Sidebar>
+        //   </Sidebar>
+        //   <div>
+        //     Content Here
+        //   </div>
+        // </Content>
 
 export default App;
