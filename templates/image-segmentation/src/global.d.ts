b63d672d819be102eed1052e12c4c13cb73884a9
Daniel Rasmuson <dan123911@gmail.com>
2018-10-21 17:35:41 -0700

    Removed outdated image labeling templates

--- templates/image-segmentation/src/global.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'react-leaflet-draw';
-declare module 'terraformer-wkt-parser';
--- templates/image-segmentation/src/redux.tsx
+++
@@ -1,6 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import { appReducer } from './app.reducer';
-
-const rootReduer = combineReducers({ app: appReducer });
-export const store = createStore(rootReduer);
-export const dispatch = store.dispatch;
